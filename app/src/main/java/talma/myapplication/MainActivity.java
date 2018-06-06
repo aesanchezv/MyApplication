1125d07c4adbdd9425c8e6a83f453ff0b56647a6
asanchez <jquerysystem@gmail.com>
2018-06-06 16:11:35 -0500

    Viajar de una pantalla a otra.

--- app/src/main/java/talma/myapplication/MainActivity.java
+++ app/src/main/java/talma/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package talma.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,8 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.talma);
 
-        Button bt=(Button)findViewById(R.id.btPulsar);
-        
+        final Button bt=(Button)findViewById(R.id.btPulsar);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent1);
+            }
+        });
 
 
     }
