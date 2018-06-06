60de5df5098c4c941ff6cceb4a88aac0e505bedd
Angel Sanchez Valderrama <angel.sanchez@talma.com.pe>
2018-06-06 16:01:16 -0500

    Initial commit

---
+++ app/src/main/java/talma/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package talma.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.talma);
+
+        Button bt=(Button)findViewById(R.id.btPulsar);
+        
+
+
+    }
+}
